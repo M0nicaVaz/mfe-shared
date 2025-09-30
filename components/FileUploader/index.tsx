f134f28b614e2258c131d8afb0ccc1c676cd5011
Carolina Rosatto <carolrosatto@gmail.com>
2025-09-29 21:26:19 -0300

    feat: adds FileUploader component

---
+++ components/FileUploader/index.tsx
@@ -0,0 +1,38 @@
+import { useRef, useState } from "react";
+import { Button } from "../Button";
+import styles from "./styles.module.scss";
+
+export function FileUploader() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [fileName, setFileName] = useState<string>("");
+
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setFileName(file.name);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <input
+        ref={fileInputRef}
+        type="file"
+        className={styles.fileInput}
+        style={{ display: "none" }}
+        onChange={handleChange}
+      />
+      <Button
+        label="Adicionar anexo"
+        size="small"
+        onClick={handleClick}
+        priority="secondary"
+      />
+      {fileName && <span className={styles.fileName}>{fileName}</span>}
+    </div>
+  );
+}
--- index.ts
+++ index.ts
@@ -5,3 +5,4 @@ export { IconButton } from './components/IconButton';
 export { InputSelect } from './components/InputSelect';
 export { InputText } from './components/InputText';
 export { MenuHamburgerContent } from './components/MenuHamburgerContent';
+export { FileUploader } from './components/FileUploader';
